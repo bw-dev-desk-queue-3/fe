079ab87151e0b0ac7528c88ead87eef9f5fc5946
Corbin Robb <corbinrobb@gmail.com>
2020-04-27 14:51:02 -0600

    Add create ticket

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Login from './components/Login';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Login />
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,48 +1,95 @@
-import React, { useState } from 'react';
-import { useForm } from "react-hook-form";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  username: yup.string().required('Username is required'),
+  password: yup.string().required('Password is required')
+});
+
 
 const Login = () => {
   const [ user, setUser ] = useState({ username: '', password: '' });
-  const { username, password } = user;
+  const [buttonOff, setButtonOff] = useState(true);
 
-  const onSubmit = values => {
-    console.log(values);
-    
+
+  const [errors, setErrors] = useState({
+    username: '',
+    password: ''
+  })
+
+  const validate = (e) => {
+    yup
+      .reach(schema, e.target.name)
+      .validate(e.target.value)
+      .then(valid => {
+        setErrors({
+          ...errors,
+          [e.target.name]: ''
+        })
+      })
+      .catch(err => {
+        setErrors({
+          ...errors,
+          [e.target.name]: err.errors
+        })
+      })
+  }
+
+  useEffect(() => {
+    schema
+      .isValid(user)
+      .then((valid) => {
+        setButtonOff(!valid);
+      });
+  }, [user])
+
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    axios
+      .post('https://bw-dev-desk.herokuapp.com/api/login', user)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => console.log(err))
+      
+    setUser({ username: '', password: '' });
   };
 
-  const { handleSubmit, register, errors } = useForm();
+  const handleChange = (e) => {
+    e.persist();
+    setUser({ ...user, [e.target.name]: e.target.value });
+    validate(e);
+  }
+
 
   return (
-    <form className="login" onSubmit={handleSubmit(onSubmit)}>
+    <form className="login" onSubmit={onSubmit}>
       <label htmlFor='username'>
         Username
         <input
           id="username"
           name="username"
           type="text"
-          value={username}
-          ref={register({
-            required: 'Username is required'
-          })}
-          onChange={(e) => setUser({ ...user, username: e.target.value })}
+          value={user.username}
+          onChange={handleChange}
         />
       </label>
-      {errors.username && errors.username.message}
+      {errors.username && <p className="error">{errors.username}</p>}
       <label htmlFor='password'>
         Password
         <input
           id="password"
           name="password"
           type="password"
-          value={password}
-          ref={register({
-            required: 'Password is required'
-          })}
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          value={user.password}
+          onChange={handleChange}
         />
       </label>
-      {errors.password && errors.password.message}
-      <button>Login</button>
+      {errors.password && <p className="error">{errors.password}</p>}
+      <button disabled={buttonOff}>Login</button>
       <button>Sign Up!</button>
     </form>
   );
