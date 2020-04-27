de01b262f1bda73ca9a9f2185c6669759234be6f
Corbin Robb <corbinrobb@gmail.com>
2020-04-27 12:57:36 -0600

    Add validation to login and add a little bit of styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from "react-hook-form";
 
-const Login = (props) => {
-  const { username, password } = props;
+const Login = () => {
+  const [ user, setUser ] = useState({ username: '', password: '' });
+  const { username, password } = user;
+
+  const onSubmit = values => {
+    console.log(values);
+    
+  };
+
+  const { handleSubmit, register, errors } = useForm();
 
   return (
-    <form className="login" onSubmit={null}>
+    <form className="login" onSubmit={handleSubmit(onSubmit)}>
       <label htmlFor='username'>
         Username
         <input
           id="username"
           name="username"
+          type="text"
           value={username}
-          onChange={null}
+          ref={register({
+            required: 'Username is required'
+          })}
+          onChange={(e) => setUser({ ...user, username: e.target.value })}
         />
       </label>
+      {errors.username && errors.username.message}
       <label htmlFor='password'>
         Password
         <input
           id="password"
           name="password"
+          type="password"
           value={password}
-          onChange={null}
+          ref={register({
+            required: 'Password is required'
+          })}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
         />
       </label>
+      {errors.password && errors.password.message}
       <button>Login</button>
       <button>Sign Up!</button>
     </form>
