dd6a57c374526772405ce518a39030cfc55bee2e
unknown <dethgrenade@gmail.com>
2020-04-28 08:27:38 -0700

    skeleton of ticket component created

---
+++ src/components/Ticket.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Ticket = props => {
+
+    return(
+        <div className="container">
+            <div className="">
+                <h2>Title</h2>
+                <h3>Category</h3>
+            </div>
+
+            <div className="">
+                <button>Assign/Return</button>
+            </div>
+
+            <div className="">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non et harum praesentium, sint nihil tempora quis minus voluptatem? Recusandae ex dolorum aliquid neque dignissimos et facilis voluptatibus nesciunt accusamus excepturi?</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Ticket
