92ce3b30e6f52198612d6240ffeb0cfa46053e25
unknown <dethgrenade@gmail.com>
2020-04-29 09:36:53 -0700

    refactor styles and buttons on tickets. Added resolved indicator to tickets

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -7,6 +7,7 @@ const Ticket = props => {
         data,
         ticketQueue,
         setTicketQueue
+
     } = props
 
     //Checks if user has permission to resolve tickets
@@ -17,10 +18,9 @@ const Ticket = props => {
         e.preventDefault();
         const id = JSON.parse(localStorage.getItem('id'));
 
-        if(data.is_assigned){
-            axiosWithAuth().put(`/api/tickets/${data.id}`, { ...data, is_assigned: !data.is_assigned, assigned_to: 0})
+        axiosWithAuth().put(`/api/tickets/${data.id}`, { ...data, is_assigned: !data.is_assigned, assigned_to: (data.is_assigned) ? 0 : id, is_resolved: (e.target.name === 'resolve') ? true : false })
             .then(res => {
-                console.log("Update", res )
+                console.log("Update", res)
                 setTicketQueue(ticketQueue.map(ticket => {
                     return ticket.id === data.id ? res.data : ticket
                 }))
@@ -28,20 +28,8 @@ const Ticket = props => {
             })
             .catch(err => {
                 console.log({ err })
-            }) 
-        }else{
-            axiosWithAuth().put(`/api/tickets/${data.id}`, { ...data, is_assigned: !data.is_assigned, assigned_to: id})
-                .then(res => {
-                    console.log("Update", res )
-                    setTicketQueue(ticketQueue.map(ticket => {
-                        return ticket.id === data.id ? res.data : ticket
-                    }))
-    
-                })
-                .catch(err => {
-                    console.log({ err })
-                })
-            }
+            })
+
 
     }
 
@@ -49,13 +37,13 @@ const Ticket = props => {
         e.preventDefault();
 
         axiosWithAuth().delete(`/api/tickets/${data.id}`)
-        .then(res => {
-            console.log("Delete", res )
-            setTicketQueue(ticketQueue.filter(ticket => ticket.key !== data.id))
-        })
-        .catch(err => {
-            console.log({ err })
-        })
+            .then(res => {
+                console.log("Delete", res)
+                setTicketQueue(ticketQueue.filter(ticket => ticket.key !== data.id))
+            })
+            .catch(err => {
+                console.log({ err })
+            })
     }
 
     return (
@@ -65,32 +53,51 @@ const Ticket = props => {
                     <h2>{data.title}</h2>
                     <h3>{data.category}</h3>
                 </div>
+                <div className="ticket-btns">
+                    {/* Assign / Return Button */}
+                    {
+                        isAdmin &&
+                            !data.is_resolved ?
+                            <button
+                                value={data.is_assigned}
+                                onClick={clickHandler}>
+                                {/* Text of Btn wil be either Return or Assign */}
+                                {data.is_assigned ? 'Return' : 'Assign'}
+                            </button>
 
-                {
-                    isAdmin ?
-                        <button
-                            value={data.is_assigned}
-                            onClick={clickHandler}>
-                            {/* Text of Btn wil be either Return or Assign */}
-                            {data.is_assigned ? 'Return' : 'Assign'}
+                            : null
+
+
+                    }
+                    {/* Resolve Button */}
+                    {
+                        isAdmin && data.is_assigned ?
+                            <button
+                                name="resolve"
+                                onClick={clickHandler}
+                            >
+                                Resolve
                         </button>
 
-                        : null
+                            : null
 
-                        
-                }
-                {
-                    isAdmin && data.is_assigned ?
-                        <button
-                            onClick={handleDelete}
-                        >
-                            Resolve
-                        </button>
 
-                        : null
+                    }
+                    {/* Ticket Resolved indicator */}
+                    {
+                        data.is_resolved ? <p className="error">Resolved</p> : null
+                    }
+                    {/* Delete Ticket Button */}
+                    {
+                        data.is_resolved &&
+                            !data.is_assigned ?
+                            <button
+                                onClick={handleDelete}
+                            >Delete</button> :
 
-                        
-                }
+                            null
+                    }
+                </div>
             </div>
 
             <div className="ticket-body">
